32325025f974122a25b13805a333ea1366b9f9fa
Gregor Podjed <gregor@gregtech.eu>
2025-01-02 18:17:01 +0100

    Made the text a bit longer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,7 @@
 	home, at work and in the bathroom? Digital eyes are all around us, and privacy is slowly
 	disappearing.
 </p>
+<p>
+	To avoid falling into a dystopia, we stand up for the protection of privacy and the right to
+	invisibility. Say no to Big Brother!
+</p>
