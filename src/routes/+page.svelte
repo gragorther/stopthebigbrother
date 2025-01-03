84db676b7b0ee9cd291af5f31cc457b83dd78e67
Gregor Podjed <gregor@gregtech.eu>
2025-01-03 16:16:41 +0100

    Added some text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
+	import ClickableButton from '$lib/ClickableButton.svelte';
+	import DualImage from '$lib/DualImage.svelte';
 	import PageTitle from '$lib/PageTitle.svelte';
 </script>
 
-<PageTitle title="Stop the Big Brother" />
+<PageTitle title="STOP THE BIG BROTHER" />
 <p>
 	Do you know who is watching you? Who is invading your privacy? Who sees what you do on the road
 	and at home, at work and in the bathroom? Digital eyes are all around us, and privacy is slowly
@@ -12,4 +14,16 @@
 	To avoid falling into a dystopia, we stand up for the protection of privacy and the right to
 	invisibility.
 </p>
-<p>Let's stop the big brother!</p>
+<p>Let's stop the big brother! Here are some tips...</p>
+<h2>Private communication</h2>
+<p>
+	Make sure you use a messaging app capable of secure encryption, such as Threema, Element or
+	Signal.
+</p>
+<h2>Oases of privacy</h2>
+<p>You can have enjoyable moments without a mobile device with you.</p>
+<h2>Stand up and do something</h2>
+<p>
+	Don't assume the current state of privacy is set in stone, educate others about privacy and help
+	them protect theirs.
+</p>
